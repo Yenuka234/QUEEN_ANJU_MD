fec1bbccf93d6a62dbdcc888b9659da31952c395
Yenuka234 <apexyenuka@gmail.com>
2024-11-22 15:21:33 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=rUtQWS4Y#60pB8e9FQhxQYWw7ItduL1ZqX_XUaT5e6TWYrC6gk_U",
 };
